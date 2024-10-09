8eaf56a0ff6ce89cf2dda2d953a5a5e47e3206fc
Hoàng Văn Vũ <brocode2k2004@gmail.com>
2024-10-09 07:36:14 +0700

    Detail Activity UI

--- app/src/main/java/com/example/app_ecommerce/Activity/MainActivity.java
+++ app/src/main/java/com/example/app_ecommerce/Activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.app_ecommerce;
+package com.example.app_ecommerce.Activity;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.app_ecommerce.R;
 
 public class MainActivity extends AppCompatActivity {
 
