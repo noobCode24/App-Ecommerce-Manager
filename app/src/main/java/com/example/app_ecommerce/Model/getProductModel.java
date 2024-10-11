8d531d6594a6ec8359dfad4d6e0645154329b303
Hoàng Văn Vũ <brocode2k2004@gmail.com>
2024-10-11 16:23:21 +0700

    Upload_List_Product

---
+++ app/src/main/java/com/example/app_ecommerce/Model/getProductModel.java
@@ -0,0 +1,33 @@
+package com.example.app_ecommerce.Model;
+
+import java.util.List;
+
+public class getProductModel {
+    private boolean success;
+    private String message;
+    private List<ProductModel> result;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<ProductModel> getResult() {
+        return result;
+    }
+
+    public void setResult(List<ProductModel> result) {
+        this.result = result;
+    }
+}
