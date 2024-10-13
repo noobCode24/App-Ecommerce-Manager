7c036767bf5222cb578cb6e51b4c6394800baadc
Hoàng Văn Vũ <brocode2k2004@gmail.com>
2024-10-13 19:13:28 +0700

    Update_Detail

--- app/src/main/java/com/example/app_ecommerce/Activity/DetailActivity.java
+++ app/src/main/java/com/example/app_ecommerce/Activity/DetailActivity.java
@@ -1,6 +1,11 @@
 package com.example.app_ecommerce.Activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +13,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.app_ecommerce.Model.ProductModel;
 import com.example.app_ecommerce.R;
+import com.squareup.picasso.Picasso;
+
+import java.text.DecimalFormat;
 
 public class DetailActivity extends AppCompatActivity {
+    TextView txt_title, txt_price, txt_desc, txt_rate, txt_soldquantity;
+    ImageView img_pic, btnAddCart, btnBack;
+    Button btnBuyNow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +34,76 @@ public class DetailActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initView();
+        ActionBack();
+        initData();
+    }
+
+    private void initData() {
+        ProductModel productModel = (ProductModel) getIntent().getSerializableExtra("Detail");
+        txt_title.setText(productModel.getProduct_name());
+        txt_desc.setText(productModel.getProduct_desc());
+        DecimalFormat decimalFormatWithDecimal = new DecimalFormat("$#,##0.00"); // Định dạng với 2 chữ số thập phân
+        DecimalFormat decimalFormatWithoutDecimal = new DecimalFormat("$#,##0"); // Định dạng không có chữ số thập phân
+
+        // Kiểm tra nếu giá là số nguyên hay không
+        if (productModel.getPrice() % 1 == 0) {
+            // Hiển thị giá không có phần thập phân
+            txt_price.setText(decimalFormatWithoutDecimal.format(productModel.getPrice()));
+        } else {
+            // Hiển thị giá có phần thập phân
+            txt_price.setText(decimalFormatWithDecimal.format(productModel.getPrice()));
+        }
+
+        // Định dạng và hiển thị số lượng bán
+        txt_soldquantity.setText(formatSoldQuantity(productModel.getSold_quantity()));
+
+        // Định dạng và hiển thị rate
+        DecimalFormat decimalFormatRating = new DecimalFormat("#,##0.0");
+        txt_rate.setText(decimalFormatRating.format(productModel.getRating()));
+
+        int imageResourceId = getResources().getIdentifier(productModel.getImage(), "drawable", getPackageName());
+
+        if (imageResourceId != 0) {
+            // Nếu có ảnh trong drawable, hiển thị nó
+            img_pic.setImageResource(imageResourceId);
+        } else {
+            // Nếu không, lấy ảnh từ internet bằng Picasso
+            Picasso.get().load(productModel.getImage()).into(img_pic);
+        }
+    }
+
+    private String formatSoldQuantity(int quantity) {
+        if (quantity >= 1000000) {
+            // Nếu số lượng lớn hơn 1 triệu, hiển thị với đơn vị "m" (ví dụ: 1,2m)
+            return String.format("%.1fm", quantity / 1000000.0);
+        } else if (quantity >= 1000) {
+            // Nếu số lượng lớn hơn 1 ngàn, hiển thị với đơn vị "k" (ví dụ: 1,1k)
+            return String.format("%.1fk", quantity / 1000.0);
+        } else {
+            // Nếu nhỏ hơn 1000, hiển thị như bình thường
+            return String.valueOf(quantity);
+        }
+    }
+
+    private void ActionBack() {
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void initView() {
+        txt_title = findViewById(R.id.txt_title);
+        txt_price = findViewById(R.id.txt_price);
+        txt_desc = findViewById(R.id.txt_desc);
+        img_pic = findViewById(R.id.img_pic);
+        btnAddCart = findViewById(R.id.btnAddCart);
+        btnBuyNow = findViewById(R.id.btnBuyNow);
+        txt_rate = findViewById(R.id.txt_rate);
+        txt_soldquantity = findViewById(R.id.txt_soldquantity);
     }
 }
--- app/src/main/java/com/example/app_ecommerce/Adapter/CategoryAdapter.java
+++ app/src/main/java/com/example/app_ecommerce/Adapter/CategoryAdapter.java
@@ -15,7 +15,9 @@ import com.example.app_ecommerce.Activity.DetailActivity;
 import com.example.app_ecommerce.Interface.ItemClickListener;
 import com.example.app_ecommerce.Model.ProductModel;
 import com.example.app_ecommerce.R;
+import com.squareup.picasso.Picasso;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyViewHolder> {
@@ -39,19 +41,31 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyView
     public void onBindViewHolder(@NonNull CategoryAdapter.MyViewHolder holder, int position) {
         ProductModel productModel = productList.get(position);
         holder.txt_title.setText(productModel.getProduct_name());
-        holder.txt_stock.setText(String.valueOf(productModel.getStock_quantity()));
+        DecimalFormat decimalFormatWithDecimal = new DecimalFormat("$#,##0.00"); // Định dạng với 2 chữ số thập phân
+        DecimalFormat decimalFormatWithoutDecimal = new DecimalFormat("$#,##0"); // Định dạng không có chữ số thập phân
+
+        // Kiểm tra nếu giá là số nguyên hay không
         if (productModel.getPrice() % 1 == 0) {
-            holder.txt_price.setText(String.format("$%.0f", productModel.getPrice()));
+            // Hiển thị giá không có phần thập phân
+            holder.txt_price.setText(decimalFormatWithoutDecimal.format(productModel.getPrice()));
         } else {
-            holder.txt_price.setText(String.format("$%.2f", productModel.getPrice()));
+            // Hiển thị giá có phần thập phân
+            holder.txt_price.setText(decimalFormatWithDecimal.format(productModel.getPrice()));
         }
 
-        // Lấy ảnh từ drawable bằng resource ID
-        int imageResourceId = context.getResources().getIdentifier(productModel.getImage(), "drawable", context.getPackageName());
-        holder.item_image.setImageResource(imageResourceId);
+        // Định dạng và hiển thị số lượng bán
+        holder.txt_soldquantity.setText("Đã bán: " + formatSoldQuantity(productModel.getSold_quantity()));
 
-        // lấy ảnh từ internet
-        //Picasso.get().load(productList.get(position).getImage()).into(holder.item_image);
+        // Kiểm tra ảnh từ drawable
+        int imageResourceId = context.getResources().getIdentifier(productModel.getImage(), "drawable", context.getPackageName());
+
+        if (imageResourceId != 0) {
+            // Nếu ảnh tồn tại trong drawable, hiển thị nó
+            holder.item_image.setImageResource(imageResourceId);
+        } else {
+            // Nếu không, lấy ảnh từ internet bằng Picasso
+            Picasso.get().load(productModel.getImage()).into(holder.item_image);
+        }
 
         holder.setItemClickListener(new ItemClickListener() {
             @Override
@@ -59,11 +73,25 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyView
                 if (!isLongclick) {
                     // click
                     Intent intent = new Intent(context, DetailActivity.class);
+                    intent.putExtra("Detail", productModel);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     context.startActivity(intent);
                 }
             }
         });
+    }
+
+    private String formatSoldQuantity(int quantity) {
+        if (quantity >= 1000000) {
+            // Nếu số lượng lớn hơn 1 triệu, hiển thị với đơn vị "m" (ví dụ: 1,2m)
+            return String.format("%.1fm", quantity / 1000000.0);
+        } else if (quantity >= 1000) {
+            // Nếu số lượng lớn hơn 1 ngàn, hiển thị với đơn vị "k" (ví dụ: 1,1k)
+            return String.format("%.1fk", quantity / 1000.0);
+        } else {
+            // Nếu nhỏ hơn 1000, hiển thị như bình thường
+            return String.valueOf(quantity);
+        }
     }
 
     @Override
@@ -72,7 +100,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyView
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        TextView txt_title, txt_price, txt_stock;
+        TextView txt_title, txt_price, txt_rate, txt_soldquantity;
         ImageView item_image;
 
         private ItemClickListener itemClickListener;
@@ -81,8 +109,8 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyView
             super(itemView);
             txt_title = itemView.findViewById(R.id.item_title);
             txt_price = itemView.findViewById(R.id.txt_price);
-            txt_stock = itemView.findViewById(R.id.txtStock);
             item_image = itemView.findViewById(R.id.item_image);
+            txt_soldquantity = itemView.findViewById(R.id.txt_Soldquantity);
             itemView.setOnClickListener(this);
         }
 
