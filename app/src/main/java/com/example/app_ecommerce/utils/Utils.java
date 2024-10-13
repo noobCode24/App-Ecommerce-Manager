46bc3d2098e8c5e2275a550b2f2dcf60155be9cd
Hoàng Văn Vũ <brocode2k2004@gmail.com>
2024-10-13 23:40:30 +0700

    Shopping_Cart

--- app/src/main/java/com/example/app_ecommerce/utils/Utils.java
+++ app/src/main/java/com/example/app_ecommerce/utils/Utils.java
@@ -1,5 +1,10 @@
 package com.example.app_ecommerce.utils;
 
+import com.example.app_ecommerce.Model.ShoppingCart;
+
+import java.util.List;
+
 public class Utils {
     public static final String BASE_URL = "http://192.168.1.7/app_Ecommerce/";
+    public static List<ShoppingCart> ShoppingCartList;
 }
