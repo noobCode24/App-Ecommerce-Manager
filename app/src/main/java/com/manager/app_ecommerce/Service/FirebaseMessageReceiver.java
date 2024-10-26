bb104ab1dcd18e731eb0c99d05bb082caf6a3e9b
Hoàng Văn Vũ <brocode2k2004@gmail.com>
2024-10-26 09:36:36 +0700

    Bell FireBase

---
+++ app/src/main/java/com/manager/app_ecommerce/Service/FirebaseMessageReceiver.java
@@ -0,0 +1,63 @@
+package com.manager.app_ecommerce.Service;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.manager.app_ecommerce.Activity.MainActivity;
+import com.manager.app_ecommerce.R;
+
+public class FirebaseMessageReceiver extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage message) {
+        if (message.getNotification() != null){
+            showNotification(message.getNotification().getTitle(), message.getNotification().getBody());
+        }
+        super.onMessageReceived(message);
+    }
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+        Log.d("FirebaseToken", "Refreshed token: " + token);
+    }
+
+    private void showNotification(String title, String body) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        String channelId = "noti";
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT  | PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), channelId)
+                .setSmallIcon(R.drawable.cloud_computing)
+                .setAutoCancel(true)
+                .setVibrate(new long[]{1000, 1000, 1000, 1000})
+                .setOnlyAlertOnce(true)
+                .setContentIntent(pendingIntent);
+        builder = builder.setContent(customView(title, body));
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(channelId, "web_app", NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        notificationManager.notify(0, builder.build());
+    }
+
+    private RemoteViews customView(String title, String body){
+        RemoteViews remoteViews = new RemoteViews(getApplicationContext().getPackageName(), R.layout.notification);
+        remoteViews.setTextViewText(R.id.title_noti,title);
+        remoteViews.setTextViewText(R.id.body_noti, body);
+        remoteViews.setImageViewResource(R.id.imgnoti, R.drawable.cloud_computing);
+        return remoteViews;
+    }
+}
